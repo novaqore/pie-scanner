dfed67dbb51918719acb5309a0828ebad9c39fc5
novaqoredev <dev@novaqore.tech>
2025-05-13 13:17:37 -0700

    update

--- api/scan.js
+++ api/scan.js
@@ -1,10 +1,10 @@
-
-
 export default function handler(req, res) {
+    const { website } = req.query;
+  
     if (req.method === 'GET') {
-      res.status(200).json({ message: 'Hello World' });
+      res.status(200).json({ message: `You sent website: ${website}` });
     } else {
       res.status(405).json({ error: 'Method Not Allowed' });
     }
-}
+  }
   
